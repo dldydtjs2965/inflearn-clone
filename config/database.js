40d4862b5df2a30684acec7e8b261264a0b6078f
dldydtjs2965 <54692225+dldydtjs2965@users.noreply.github.com>
2021-11-16 16:31:33 +0900

    docs: 프로젝트 read.me update

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
-    port: '',
-    password: '',
-    database: ''
+    host: 'inflearn-database.czzcule8mgp6.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
+    port: '3306',
+    password: 'inflearn1234!',
+    database: 'inflearn'
 });
 
 module.exports = {
